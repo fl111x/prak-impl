0745fdda9439c7a1a075b306d411d8ff44cb3961
fl111x <ammar.bfw@gmail.com>
2026-01-08 13:06:24 +0700

    update: p8

---
+++ p8/tugas 7.6.js
@@ -0,0 +1,285 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"tugas 7.6_atlas_1", frames: [[0,20,904,4],[0,0,908,8],[0,10,908,8]]}
+];
+
+
+(lib.AnMovieClip = function(){
+	this.actionFrames = [];
+	this.ignorePause = false;
+	this.gotoAndPlay = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndPlay.call(this,positionOrLabel);
+	}
+	this.play = function(){
+		cjs.MovieClip.prototype.play.call(this);
+	}
+	this.gotoAndStop = function(positionOrLabel){
+		cjs.MovieClip.prototype.gotoAndStop.call(this,positionOrLabel);
+	}
+	this.stop = function(){
+		cjs.MovieClip.prototype.stop.call(this);
+	}
+}).prototype = p = new cjs.MovieClip();
+// symbols:
+
+
+
+(lib.CachedBmp_4 = function() {
+	this.initialize(ss["tugas 7.6_atlas_1"]);
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_5 = function() {
+	this.initialize(ss["tugas 7.6_atlas_1"]);
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.CachedBmp_1 = function() {
+	this.initialize(ss["tugas 7.6_atlas_1"]);
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+// stage content:
+(lib.tugas76 = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Body
+	this.text = new cjs.Text("Jenderal Achmad Yani University | Cimahi, Indonesia \nBachelor of informatics| estimated pass : August 2026\nCurrent GPA: 3.63 / 4.00", "14px 'Times New Roman'");
+	this.text.lineHeight = 18;
+	this.text.lineWidth = 447;
+	this.text.parent = this;
+	this.text.setTransform(48.15,662.5);
+
+	this.text_1 = new cjs.Text("Education", "bold 14px 'Times New Roman'");
+	this.text_1.textAlign = "center";
+	this.text_1.lineHeight = 18;
+	this.text_1.lineWidth = 69;
+	this.text_1.parent = this;
+	this.text_1.setTransform(82.9,632);
+
+	this.instance = new lib.CachedBmp_5();
+	this.instance.setTransform(45.15,651,0.5,0.5);
+
+	this.text_2 = new cjs.Text("Assistant Lecturer in Computer Organization & Architecture \nJenderal Achmad Yani University\n-Assist lecturers in facilitating teaching and learning activities for Computer Organization & Architecture courses.\n-Deliver lecture material and lead weekly practical sessions for 30 students.\n-Provide guidance and assistance to students in understanding core concepts such as instruction set architecture (ISA), pipelines, and memory hierarchies.\n-Assist in the preparation, distribution, and grading of assignments and quizzes to ensure student understanding of the material.", "12px 'Times New Roman'");
+	this.text_2.lineHeight = 15;
+	this.text_2.lineWidth = 449;
+	this.text_2.parent = this;
+	this.text_2.setTransform(48.15,458.7);
+
+	this.text_3 = new cjs.Text("Experience", "bold 14px 'Times New Roman'");
+	this.text_3.textAlign = "center";
+	this.text_3.lineHeight = 18;
+	this.text_3.lineWidth = 77;
+	this.text_3.parent = this;
+	this.text_3.setTransform(86.95,428.2);
+
+	this.instance_1 = new lib.CachedBmp_4();
+	this.instance_1.setTransform(46.15,448.2,0.5,0.5);
+
+	this.text_4 = new cjs.Text("Non-technical \n-skillsLeadership \n-AbilityGood \n-communicationAdapt to the new environment\n-Easy to Collaborate with\n-Thorough", "14px 'Times New Roman'");
+	this.text_4.lineHeight = 18;
+	this.text_4.lineWidth = 267;
+	this.text_4.parent = this;
+	this.text_4.setTransform(234.6,235.5);
+
+	this.text_5 = new cjs.Text("Technical skillsProgramming language :  \n- PHP  \n- Java  \n- C          \n- Python\n\nWeb development :         - HTML, CSS, JavaScript         - NodeJS", "14px 'Times New Roman'");
+	this.text_5.lineHeight = 18;
+	this.text_5.lineWidth = 172;
+	this.text_5.parent = this;
+	this.text_5.setTransform(48.15,235.5);
+
+	this.text_6 = new cjs.Text("Skills", "bold 14px 'Times New Roman'");
+	this.text_6.textAlign = "center";
+	this.text_6.lineHeight = 18;
+	this.text_6.lineWidth = 45;
+	this.text_6.parent = this;
+	this.text_6.setTransform(71.15,205);
+
+	this.instance_2 = new lib.CachedBmp_5();
+	this.instance_2.setTransform(45.15,224,0.5,0.5);
+
+	this.text_7 = new cjs.Text("I am Ammar Bagas Fathurrahman Wantoro, a fifth-semester Informatics student at Jenderal Achmad Yani University. Passionate about software engineering with focus on Frontend, Backend, and Data Analysis.", "14px 'Times New Roman'");
+	this.text_7.lineHeight = 18;
+	this.text_7.lineWidth = 447;
+	this.text_7.parent = this;
+	this.text_7.setTransform(48.15,125.8);
+
+	this.text_8 = new cjs.Text("About", "bold 14px 'Times New Roman'");
+	this.text_8.textAlign = "center";
+	this.text_8.lineHeight = 18;
+	this.text_8.lineWidth = 45;
+	this.text_8.parent = this;
+	this.text_8.setTransform(71.15,95.3);
+
+	this.instance_3 = new lib.CachedBmp_5();
+	this.instance_3.setTransform(45.15,114.3,0.5,0.5);
+
+	this.instance_4 = new lib.CachedBmp_1();
+	this.instance_4.setTransform(45.15,450.95,0.5,0.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_4},{t:this.instance_3},{t:this.text_8},{t:this.text_7},{t:this.instance_2},{t:this.text_6},{t:this.text_5},{t:this.text_4},{t:this.instance_1},{t:this.text_3},{t:this.text_2},{t:this.instance},{t:this.text_1},{t:this.text}]}).wait(1));
+
+	// header
+	this.text_9 = new cjs.Text("+6282119763858 | ammar.bfw@gmail.com |  linkedin.com/in/ammar-bagas-fw/", "10px 'Times New Roman'", "#1E1E1E");
+	this.text_9.textAlign = "center";
+	this.text_9.lineHeight = 13;
+	this.text_9.lineWidth = 321;
+	this.text_9.parent = this;
+	this.text_9.setTransform(225.6,58.4);
+
+	this.text_10 = new cjs.Text("Ammar Bagas Fathurrahman Wantoro", "bold 22px 'Times New Roman'");
+	this.text_10.textAlign = "center";
+	this.text_10.lineHeight = 26;
+	this.text_10.lineWidth = 411;
+	this.text_10.parent = this;
+	this.text_10.setTransform(254,31.1);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.text_10},{t:this.text_9}]}).wait(1));
+
+	// bg
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("rgba(0,0,0,0)").ss(1,1,1).p("EguwhCHMBdhAAAMAAACEPMhdhAAAg");
+	this.shape.setTransform(299.25,423.225);
+
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("#FFFFFF").s().p("EguwBCIMAAAiEPMBdhAAAMAAACEPg");
+	this.shape_1.setTransform(299.25,423.225);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape_1},{t:this.shape}]}).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = p = new lib.AnMovieClip();
+p.nominalBounds = new cjs.Rectangle(298,422,301.5,425.5);
+// library properties:
+lib.properties = {
+	id: '95B3CCED8DDDB441990C15F1E505F569',
+	width: 598,
+	height: 846,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/tugas 7.6_atlas_1.png?1766669853058", id:"tugas 7.6_atlas_1"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['95B3CCED8DDDB441990C15F1E505F569'] = {
+	getStage: function() { return exportRoot.stage; },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
+	var lastW, lastH, lastS=1;		
+	window.addEventListener('resize', resizeCanvas);		
+	resizeCanvas();		
+	function resizeCanvas() {			
+		var w = lib.properties.width, h = lib.properties.height;			
+		var iw = window.innerWidth, ih=window.innerHeight;			
+		var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;			
+		if(isResp) {                
+			if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {                    
+				sRatio = lastS;                
+			}				
+			else if(!isScale) {					
+				if(iw<w || ih<h)						
+					sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==1) {					
+				sRatio = Math.min(xRatio, yRatio);				
+			}				
+			else if(scaleType==2) {					
+				sRatio = Math.max(xRatio, yRatio);				
+			}			
+		}
+		domContainers[0].width = w * pRatio * sRatio;			
+		domContainers[0].height = h * pRatio * sRatio;
+		domContainers.forEach(function(container) {				
+			container.style.width = w * sRatio + 'px';				
+			container.style.height = h * sRatio + 'px';			
+		});
+		stage.scaleX = pRatio*sRatio;			
+		stage.scaleY = pRatio*sRatio;
+		lastW = iw; lastH = ih; lastS = sRatio;            
+		stage.tickOnUpdate = false;            
+		stage.update();            
+		stage.tickOnUpdate = true;		
+	}
+}
+an.handleSoundStreamOnTick = function(event) {
+	if(!event.paused){
+		var stageChild = stage.getChildAt(0);
+		if(!stageChild.paused || stageChild.ignorePause){
+			stageChild.syncStreamSounds();
+		}
+	}
+}
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
